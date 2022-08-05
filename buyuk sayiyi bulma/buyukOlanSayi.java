e72dcbec85f7ba8f113293e9594b8df483b23e3e
Hasan Hüseyin Çokyürür <82513466+BrizeTempite-scripter@users.noreply.github.com>
2022-08-05 11:16:32 +0300

    Add files via upload

---
+++ buyuk sayiyi bulma/buyukOlanSayi.java
@@ -0,0 +1,16 @@
+public class buyukOlanSayi {
+  public static void main(String[] args) {
+    int sayi1=10;
+    int sayi2 =30;
+    int sayi3 =40;
+    if(sayi1>sayi2&&sayi1>sayi3){
+      System.out.println("en buyuk sayi sayi1 dir");
+    }
+    else if (sayi2>sayi1&&sayi2>sayi3){
+      System.out.println("en buyuk sayi sayi2 dir");
+    }
+    else {
+      System.out.println("en buyuk sayi sayi3 dür");
+    }
+  }
+}
