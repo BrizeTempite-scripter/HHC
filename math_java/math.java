2194843ac9592f13b91e58e59c0b51ba2ffc75b9
Hasan Hüseyin Çokyürür <82513466+BrizeTempite-scripter@users.noreply.github.com>
2022-08-14 09:16:47 +0300

    Add files via upload

---
+++ math_java/math.java
@@ -0,0 +1,23 @@
+public class math {
+  public static double topla(double x,double y ){
+    return x+y;
+  }
+  public static double cikar(double x,double y ){
+    return x+y;
+  }
+  public static double bol(double x,double y ){
+    return x+y;
+  }
+  public static double carp(double x,double y ){
+    return x+y;
+  }
+  public static double abs(double x){
+    if(x<0){
+      x=x*-1;
+    }
+  return x;
+  }
+  public static void main(String[] args) {
+    
+  }
+}
